f2bcab034a6b0d6e45e8e7fa56aead34a823a44c
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2020-07-30 12:04:32 -0500

    Mejora de codigo.

--- Challenge/Emmanuel_Hernandez_Lira/script.js
+++ Challenge/Emmanuel_Hernandez_Lira/script.js
@@ -1,40 +1,18 @@
-const promise1 = new Promise((resolve, reject) => {
-  const rand = Math.floor(Math.random() * 100) + 1;
+const getPromise = () =>
+  new Promise((resolve, reject) => {
+    const rand = Math.floor(Math.random() * 100) + 1;
 
-  console.log(rand);
+    console.log(rand);
 
-  if (rand > 50) {
-    resolve();
-  } else {
-    reject();
-  }
-});
+    rand > 50 ? resolve() : reject();
+  });
 
-const promise2 = new Promise((resolve, reject) => {
-  const rand = Math.floor(Math.random() * 100) + 1;
+const promise1 = getPromise();
+const promise2 = getPromise();
+const promise3 = getPromise();
+const promises = [promise1, promise2, promise3];
 
-  console.log(rand);
-
-  if (rand > 50) {
-    resolve();
-  } else {
-    reject();
-  }
-});
-
-const promise3 = new Promise((resolve, reject) => {
-  const rand = Math.floor(Math.random() * 100) + 1;
-
-  console.log(rand);
-
-  if (rand > 50) {
-    resolve();
-  } else {
-    reject();
-  }
-});
-
-Promise.all([promise1, promise2, promise3])
+Promise.all(promises)
   .then(() => {
     console.log("¡Éxito!");
     console.log("Todos los números son mayores a 50.");
